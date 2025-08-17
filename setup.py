ddf2dfb8481862d2ac3edfd63bbb30e264f7029f
diesel <diesel.wolf@yandex.ru>
2025-08-18 01:51:32 +0300

    v1.0.3: Fix GitHub Actions release workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="fastwg",
-    version="1.0.2",
+    version="1.0.3",
     author="wolfDiesel",
     author_email="",
     description="Fast WireGuard server management via CLI",
