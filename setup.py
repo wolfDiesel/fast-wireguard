c5c7102e4042f18620abd7686bb197d195035ecd
diesel <diesel.wolf@yandex.ru>
2025-08-18 01:54:20 +0300

    Update version to v1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="fastwg",
-    version="1.0.3",
+    version="1.0.4",
     author="wolfDiesel",
     author_email="",
     description="Fast WireGuard server management via CLI",
